c6a1de1c269515c6133a6cae87918a02daf1e1d7
Michael Plank <michael.plank@canterbury.ac.nz>
2023-09-01 14:04:32 +1200

    Update calcLogLikImproved.m
    Minor comment improvements

--- functions/calcLogLikImproved.m
+++ functions/calcLogLikImproved.m
@@ -10,15 +10,16 @@ nPoints = length(obs);
 
 % Construct a modified parameter structure by overwriting the default settings with the specified values of Theta
 par = getTrialParImproved(Theta, par);
-par.pObs = 1;
 
-% Solve forward model
+% Solve forward model with pObs = 1
+par.pObs = 1;
 sol = solveModel(par); 
 
 % Expected observed values under pObs=1
 Yt = 1/par.tObs * sol.C1;       
 
 % Find optimal pObs and associated log likelihood, starting from initial guess pObs = 0.5
+% Note this optimisation stop does not require the forward model to be re-run
 objFn = @(pObs)(-nestedObjFn(pObs, Yt, obs, par));
 [pObsOpt, f, exitFlag, output] = fmincon(objFn, 0.5, [], [], [], [], 0, 1, [], options);
 assert(exitFlag > 0)
