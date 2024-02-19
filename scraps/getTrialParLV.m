305db5a395854d60481ba6bafc9521764cd57f62
Michael Plank <michael.plank@canterbury.ac.nz>
2024-02-19 19:58:47 +1300

    Removing old files

--- scraps/getTrialParLV.m
+++
@@ -1,9 +0,0 @@
-function trialPar = getTrialParLV(Theta, par)
-
-% Construct a modified (trial) parameter structure trialPar by overwriting the default settings in par with the specified values of Theta
-
-trialPar = par;
-trialPar.r  = Theta(1);
-trialPar.a = Theta(2);
-trialPar.pObs = Theta(3);
-%trialPar.obsSD = Theta(4);
--- scraps/getTrialParSEIR.m
+++
@@ -1,9 +0,0 @@
-function trialPar = getTrialParSEIR(Theta, par)
-
-% Construct a modified (trial) parameter structure trialPar by overwriting the default settings in par with the specified values of Theta
-
-trialPar = par;
-trialPar.R0  = Theta(1);
-trialPar.tR = Theta(2);
-trialPar.pObs = Theta(3);
-trialPar.obsSD = Theta(4);
