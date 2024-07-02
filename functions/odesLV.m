f6a16e4d78eba6ea6688d235fa6f6dcb0e6c5e35
Michael Plank <michael.plank@canterbury.ac.nz>
2024-07-02 13:00:34 +0100

    Update odesLV.m
    Added comment

--- functions/odesLV.m
+++ functions/odesLV.m
@@ -6,6 +6,7 @@ dydt = [par.r*y(1)*(1-y(1)/par.K) - par.a*y(1)*y(2)/(y(1)+par.b);
         par.a*y(1)*y(2)/(y(1)+par.b) - par.mu*y(2)  ];
 
 % To prevent variables from becoming negative:
+% (note an altenrative way to do this that avoids artificially introducing a steep gradient in the solution when outside the bioloigcal region would be to adjust error tolerances so that negative solutions never occur in the parameter range of interest)
 negFlag = y < 0;
 dydt(negFlag) = -big*y(negFlag);
 
